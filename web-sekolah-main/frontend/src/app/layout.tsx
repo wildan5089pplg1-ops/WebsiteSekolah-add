305702ce1a21fe1de6aca14ac0e639fb4ffd2b0f
rajendraonc <rajendraonc@gmail.com>
2026-09-23 12:50:34 +0700

    navbar update 2

--- web-sekolah-main/frontend/src/app/layout.tsx
+++ web-sekolah-main/frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/Navbar_old';
+import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import ChatBot from '@/components/ChatBot';
